1ed458cec2ffc023640524a82d6b748d3f05e4a4
Brawek <113200223+Brawek@users.noreply.github.com>
2025-11-16 22:11:46 +0100

    Add files via upload

---
+++ src/myclass/app/ShapeApp.java
@@ -0,0 +1,11 @@
+package myclass.app;
+
+import myclass.gui.Start;
+
+public class ShapeApp {
+    public static void main(String[] args) {
+        System.out.printf("Start");
+        Start window = new Start("Baza Figur");
+
+    }
+}
